f2d92751805f709d9ec34d9ece45a2c36d704844
Ricard Penin Honrubia <ergoegos@gmail.com>
2024-03-04 00:05:00 +0100

    ui character info and card animation

---
+++ src/App.svelte
@@ -0,0 +1,85 @@
+<script>
+  import Character from "./lib/Character.svelte";
+
+  let info = {};
+  let characters = [];
+  let page = 9;
+
+  async function loadCharacters() {
+    const response = await fetch(
+      "https://rickandmortyapi.com/api/character?page=" + page,
+    );
+    const data = await response.json();
+    info = data.info;
+    characters = data.results;
+    //  console.log(info.pages)
+  }
+
+  function nextPage() {
+    page++;
+    loadCharacters();
+  }
+  function prevPage() {
+    page--;
+    loadCharacters();
+  }
+
+  loadCharacters();
+</script>
+
+<h1>Rick and Morty API Consume</h1>
+<hr />
+<h2>{info.count} characters in {info.pages} pages</h2>
+<header>
+  <button on:click={prevPage} disabled={page === 1}>Prev</button>
+  <h3>{page} / {info.pages}</h3>
+  <button on:click={nextPage} disabled={page === info.pages}>Next</button>
+</header>
+
+<main class="characters-container">
+  {#each characters as character}
+    <Character {character} />
+  {/each}
+</main>
+
+<header>
+  <button on:click={prevPage} disabled={page === 1}>Prev</button>
+  <h3>{page} / {info.pages}</h3>
+  <button on:click={nextPage} disabled={page === info.pages}>Next</button>
+</header>
+
+<style>
+  header {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-around;
+    align-items: baseline;
+    padding: 2rem;
+  }
+  button {
+    background-color: #213547;
+    color: #fff;
+  }
+
+  main {
+    max-width: 720px;
+
+  }
+
+
+  @media screen and (orientation: portrait) {
+    .characters-container {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 2rem;
+    }
+  }
+
+  @media screen and (orientation: landscape) {
+    .characters-container {
+      display: grid;
+      grid-template-columns: repeat(4, 1fr);
+      gap: 2rem;
+    }
+  }
+</style>
---
+++ src/lib/Character.svelte
@@ -0,0 +1,127 @@
+<script>
+  export let character;
+
+  function toggle() {
+    let img = document.getElementById(character.id).children[0];
+    img.classList.toggle("close-img");
+
+    let article = document.getElementById(character.id).children[2];
+    article.classList.toggle("show-article");
+  }
+
+  setTimeout(
+    function () {
+      toggle();
+    },
+    Math.floor(Math.random() * 1000),
+  );
+  console.log(character);
+</script>
+
+<button id={character.id} class="character" on:click={toggle}>
+  <span class="close-img">
+    <img
+      id="{character.id}-img"
+      src={character.image}
+      alt={character.species}
+    />
+  </span>
+
+  <h3>{character.name}</h3>
+
+  <article class="show-article">
+    <p>#{character.species}</p>
+    <p>#{character.status}</p>
+
+    <sup><i>{character.location.name}</i></sup>
+
+    <p></p>
+  </article>
+</button>
+
+<style>
+  i {
+    font-weight: lighter;
+  }
+  h3 {
+    font-size: 1em;
+    padding: 16px;
+    margin: auto;
+    text-shadow: 1px 1px 2px black;
+  }
+  img {
+    border-top-right-radius: 8px;
+    border-top-left-radius: 8px;
+    width: 100%;
+    height: 100%;
+  }
+
+  span {
+    display: block;
+    overflow: hidden;
+    width: 100%;
+    height: 256px;
+    -webkit-transition: height .05s ease-in-out;
+    -moz-transition: height .05s ease-in-out;
+    -ms-transition: height .05s ease-in-out;
+    -o-transition: height .05s ease-in-out;
+    transition: height .05s ease-in-out;
+  }
+  .close-img {
+    height: 0px;
+  }
+
+  @media screen and (orientation: portrait) {
+    .character {
+      width: 40vw;
+      height: 64vw;
+    }
+  }
+
+  @media screen and (orientation: landscape) {
+    .character {
+      width: 160px;
+      height: 256px;
+    }
+  }
+
+  .character {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    overflow: hidden;
+    margin-bottom: 0px;
+    padding: 0;
+    background-color: #213547;
+    color: #fff;
+    border: none;
+    border-radius: 8px;
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.5);
+    filter: brightness(100%);
+
+    transform: scale(1);
+    -webkit-transition: transform .05s ease-in-out;
+    -moz-transition: transform .05s ease-in-out;
+    -ms-transition: transform .05s ease-in-out;
+    -o-transition: transform .05s ease-in-out;
+    transition: transform .05s ease-in-out;
+
+ 
+  }
+
+  .character:hover {
+    cursor: pointer;
+    filter: brightness(108%);
+    transform: scale(1.05);
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.75);
+  }
+
+  article {
+    display: none;
+  }
+
+  .show-article {
+    display: block;
+    widows: 100%;
+  }
+</style>
